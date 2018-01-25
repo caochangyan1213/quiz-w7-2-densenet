7d3ca5dea7da4443791fbf4a1c130f26ff64d614
dwSun <dwSun@Capsule>
2018-01-25 10:29:52 +0800

    fix checkpoint for quiz densenet

--- train_eval_image_classifier.py
+++ train_eval_image_classifier.py
@@ -11,7 +11,7 @@ def parse_args(check=True):
     # train
     parser.add_argument('--dataset_name', type=str, default='quiz')
     parser.add_argument('--dataset_dir', type=str)
-    parser.add_argument('--checkpoint_path', type=str)
+    parser.add_argument('--checkpoint_path', type=str, default='')
     parser.add_argument('--model_name', type=str, default='inception_v4')
     parser.add_argument('--checkpoint_exclude_scopes', type=str, default='InceptionV4/Logits,InceptionV4/AuxLogits/Aux_logits')
     parser.add_argument('--train_dir', type=str)
@@ -29,7 +29,7 @@ def parse_args(check=True):
     return FLAGS, unparsed
 
 
-train_cmd = 'python ./train_image_classifier.py  --dataset_name={dataset_name} --dataset_dir={dataset_dir} --checkpoint_path={checkpoint_path} --model_name={model_name} --checkpoint_exclude_scopes={checkpoint_exclude_scopes} --train_dir={train_dir} --learning_rate={learning_rate} --optimizer={optimizer} --batch_size={batch_size} --max_number_of_steps={max_number_of_steps} --clone_on_cpu={clone_on_cpu}'
+train_cmd = 'python ./train_image_classifier.py  --dataset_name={dataset_name} --dataset_dir={dataset_dir} --model_name={model_name} --checkpoint_exclude_scopes={checkpoint_exclude_scopes} --train_dir={train_dir} --learning_rate={learning_rate} --optimizer={optimizer} --batch_size={batch_size} --max_number_of_steps={max_number_of_steps} --clone_on_cpu={clone_on_cpu}'
 eval_cmd = 'python ./eval_image_classifier.py --dataset_name={dataset_name} --dataset_dir={dataset_dir} --dataset_split_name={dataset_split_name} --model_name={model_name}   --checkpoint_path={checkpoint_path}  --eval_dir={eval_dir} --batch_size={batch_size}  --max_num_batches={max_num_batches}'
 
 if __name__ == '__main__':
@@ -40,15 +40,20 @@ if __name__ == '__main__':
     os.chdir(w_d)
 
     step_per_epoch = 50000 // FLAGS.batch_size
+
+    if FLAGS.checkpoint_path:
+        ckpt = ' --checkpoint_path=' + FLAGS.checkpoint_path
+    else:
+        ckpt = ''
     for i in range(30):
         steps = int(step_per_epoch * (i + 1))
         # train 1 epoch
         print('################    train    ################')
         p = os.popen(train_cmd.format(**{'dataset_name': FLAGS.dataset_name, 'dataset_dir': FLAGS.dataset_dir,
-                                         'checkpoint_path': FLAGS.checkpoint_path, 'model_name': FLAGS. model_name,
+                                         'model_name': FLAGS. model_name,
                                          'checkpoint_exclude_scopes': FLAGS.checkpoint_exclude_scopes, 'train_dir': FLAGS. train_dir,
                                          'learning_rate': FLAGS.learning_rate, 'optimizer': FLAGS.optimizer,
-                                         'batch_size': FLAGS.batch_size, 'max_number_of_steps': steps, 'clone_on_cpu':FLAGS.clone_on_cpu}))
+                                         'batch_size': FLAGS.batch_size, 'max_number_of_steps': steps, 'clone_on_cpu': FLAGS.clone_on_cpu}) + ckpt)
         for l in p:
             print(p.strip())
 
